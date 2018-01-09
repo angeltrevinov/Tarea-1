fb814c41c42d34ad2a1e4e1e8f9e7fbfeea3cb73
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-01-09 12:33:07 -0600

    Starting code

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+  Angel Odiel Treviño Villanueva
+  A01336559
+  Tarea: 1 Fecha: 9/Enero Materia: Diseño de Algoritmos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  /* Code Here */
+
+  return 0;
+}
