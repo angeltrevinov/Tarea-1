9a2680f85775d7adb4e29da33c7bd7aa6000f048
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-01-09 18:25:32 -0600

    fixing things up

--- main.cpp
+++ main.cpp
@@ -11,13 +11,20 @@ using namespace std;
 
 int main(){
 
-  string Entrada = "AABCCDDEEFF";
+  string Entrada = "";//AABCCDDEEFF
+  cout << "entrada"<< endl;
+  cin >> Entrada;
+  bool found = false;
 
-  for(int i = 0; i < Entrada.length()-1; i+=2){ //se mueve de dos en dos
-    if(Entrada[i] != Entrada[i+1] ){ //cuando encuentra el unico se detiene
+  for(int i = 0; i < Entrada.length()-1 && found == false; i+=2){ //se mueve de dos en dos
+    if(Entrada[i] != Entrada[i+1]){ //cuando encuentra el unico se detiene
       cout << Entrada[i] << endl;
-      break;
+      found = true;
     }
+  }
+
+  if(found == false){
+    cout << "no encontrado" << endl;
   }
 
   return 0;
